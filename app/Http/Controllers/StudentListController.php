7d8e25cd087f7dcd47f4a6d775c23bbe507e8e15
Unknown <www.limsp95@gmail.com>
2019-05-23 16:10:58 +0800

    added students list model ( -a)

---
+++ app/Http/Controllers/StudentListController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StudentList;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StudentListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StudentList  $studentList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StudentList $studentList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StudentList  $studentList
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StudentList $studentList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StudentList  $studentList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StudentList $studentList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StudentList  $studentList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StudentList $studentList)
+    {
+        //
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -506,6 +506,7 @@ class ComposerStaticInit974dc00bd043fa2d2fd91f3dc7a52d8b
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
         'App\\Providers\\EventServiceProvider' => __DIR__ . '/../..' . '/app/Providers/EventServiceProvider.php',
         'App\\Providers\\RouteServiceProvider' => __DIR__ . '/../..' . '/app/Providers/RouteServiceProvider.php',
+        'App\\StudentList' => __DIR__ . '/../..' . '/app/StudentList.php',
         'App\\User' => __DIR__ . '/../..' . '/app/User.php',
         'BeyondCode\\DumpServer\\DumpServerCommand' => __DIR__ . '/..' . '/beyondcode/laravel-dump-server/src/DumpServerCommand.php',
         'BeyondCode\\DumpServer\\DumpServerServiceProvider' => __DIR__ . '/..' . '/beyondcode/laravel-dump-server/src/DumpServerServiceProvider.php',
